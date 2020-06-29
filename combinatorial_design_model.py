c2aaccb42d84b6ff9b3a357c16d9cb373a092ee7
Hamed <eramian@netrias.com>
2020-06-29 15:50:14 -0400

    CDM init creates current/future dataframes. Major class restructuring. - CDM class init creates `self.existing_data` and `self.future_data` by   calling `self.generate_future_experiments_df`. - removed `variable_columns` from each method and made it a class   argument called `exp_condition_cols` since the conditions will be   the same for each CDM instance. - renamed `retrieve_experiments_to_predict` to   `retrieve_future_experiments` and minor code changes for cleaner code. - removed `return_sampled_df` and replaced it with sklearn's   train_test_split because it is more robust, cleaner, and   can do stratified sampling. - created `run_single` method which creates a single train/test split   and will invoke Test Harness 1 time (this is not finished yet). - updated progressive sampling method to include end_percent and use   stratified sampling via train_test_split. - removed duplicate methods that were added by accident. - renamed some variables and method names to better reflect what they do
    Former-commit-id: ebcd900d647053711ed400fdabe52ae3714a093a

--- combinatorial_design_model.py
+++ combinatorial_design_model.py
@@ -1,6 +1,7 @@
-import pandas as pd
-import numpy as np
+import os
 import itertools
+import numpy as np
+import pandas as pd
 from harness.test_harness_class import TestHarness
 from CDM_regression import CDM_regression_model
 from CDM_classification import CDM_classification_model
@@ -12,26 +13,95 @@ from scipy.stats import wasserstein_distance
 
 
 class CombinatorialDesignModel:
-
-    def __init__(self, initial_data="", path=".", query={}, **th_properties):
+    # TODO: let user define train_ratio (default to 0.7)
+    def __init__(self, initial_data=None, path=".", leaderboard_query=None, exp_condition_cols=None, **th_properties):
         # TODO: build a way to check if user wants to run something that they already ran.
         # TODO: and read that instead of re-running test harnness
 
         self.path = path
         self.th = TestHarness(output_location=path)
-        if not query:
-            ## TODO: Hamed, update this to call William's methods to generate train/test/predicted untested dataframe based on condition space
-            self.data = initial_data  # this is both train/test
-            self.left_out_conditions = None  # this is the dataframe for left out conditions and will serve as predict_untested_df
-            self.build_model(th_properties)
+
+        if exp_condition_cols is None:
+            self.exp_condition_cols = ["strain_name", "inducer_concentration_mM"]
         else:
-            self.model_id = query_leaderboard(query=query, th_output_location=path)[Names_TH.RUN_ID]
+            self.exp_condition_cols = exp_condition_cols
+
+        if leaderboard_query is None:
+            # the data is split into existing_data and future_data
+            self.existing_data = initial_data
+            self.future_data = self.generate_future_experiments_df()
+        else:
+            self.model_id = query_leaderboard(query=leaderboard_query, th_output_location=path)[Names_TH.RUN_ID]
             # Put in code to get the path of the model and read it in once Hamed works that in
             self.model = ""
 
+    def retrieve_future_experiments(self):
+        """
+        computes full condition space and returns experiments that have no data and need to be predicted
+        """
+        unique_column_values = []
+        for col in self.exp_condition_cols:
+            col_unique_vals = self.existing_data[col].unique()
+            print('Column {0}: contains {1} unique values'.format(col, len(col_unique_vals)))
+            unique_column_values.append(col_unique_vals)
+        permutations = set(itertools.product(*unique_column_values))
+        temp_df = self.existing_data[self.exp_condition_cols].drop_duplicates()
+        existing_experiments = set(zip(*[temp_df[column].values for column in self.exp_condition_cols]))
+        future_experiments = permutations - existing_experiments
+        existing_experiments = list(existing_experiments)
+        future_experiments = list(future_experiments)
+
+        print('Input dataframe contains {0} conditions out of {1} possible conditions'
+              '\nThere are {2} conditions to be predicted\n'.format(len(existing_experiments),
+                                                                    len(permutations),
+                                                                    len(future_experiments)))
+        return future_experiments
+
+    def generate_future_experiments_df(self):
+        """
+        generates and returns a dataframe of experiments that lack data
+        """
+        future_experiments = self.retrieve_future_experiments()
+        future_experiments_df = pd.DataFrame(future_experiments, columns=self.exp_condition_cols)
+        return future_experiments_df
+
+    def run_single(self, percent_train=70):
+        """
+        Generates a train/test split of self.existing_data based on the passed-in percent_train amount,
+        and runs a single test harness model on that split by calling self.invoke_test_harness.
+        The split is stratified on self.exp_condition_cols.
+        """
+        train_ratio = percent_train / 100.0
+        train_df, test_df = train_test_split(self.existing_data, train_size=train_ratio, random_state=5,
+                                             stratify=self.existing_data[self.exp_condition_cols])
+        # note this is not finished
+
+    def run_progressive_sampling(self, num_runs=3, start_percent=25, end_percent=100, step_size=5,
+                                 percent_train=70):
+        train_ratio = percent_train / 100.0
+        percent_list = list(range(start_percent, end_percent, step_size))
+        if end_percent not in percent_list:
+            percent_list.append(end_percent)
+        for run in range(1, num_runs):
+            for percent in percent_list:
+                print(percent)
+                if percent == 100:
+                    existing_data_sample = self.existing_data.copy()
+                else:
+                    ratio = percent / 100.0
+                    print(ratio)
+                    # the following line samples data from self.existing_data with stratification and a set random_state:
+                    existing_data_sample, _ = train_test_split(self.existing_data, train_size=percent, random_state=5,
+                                                               stratify=self.existing_data[self.exp_condition_cols])
+                # now we split the existing_data_sample into train and test, with stratification and a set random_state:
+                train_df, test_df = train_test_split(existing_data_sample, train_size=train_ratio, random_state=5,
+                                                     stratify=existing_data_sample[self.exp_condition_cols])
+                self.build_model(train_df=train_df, test_df=test_df, th_properties={})
+            # TODO: characterizaton has yet to include calculation of knee point
+
     def build_model(self, train_df=None, test_df=None, th_properties={}):
         if train_df is None or test_df is None:
-            train_df, test_df = train_test_split(self.data, test_size=0.2)
+            train_df, test_df = train_test_split(self.existing_data, test_size=0.2)
 
         # TODO: Fix up test/harness run
         '''
@@ -41,56 +111,13 @@ class CombinatorialDesignModel:
                            dict_of_function_parameters={'input_size': len(th_properties['feature_cols_to_use']),
                                                         'output_size': len(th_properties['cols_to_predict'])},
                            training_data=train_df, testing_data=test_df,
-                           data_and_split_description=th_properties[Names_TH.DATA_AND_SPLIT_DESCRIPTION],
-                           cols_to_predict=th_properties['cols_to_predict'], feature_cols_to_use=th_properties['feature_cols_to_use'],
-                           index_cols=th_properties['index_cols'], normalize=False, feature_cols_to_normalize=None)
+                           description=th_properties[Names_TH.DESCRIPTION],
+                           target_cols=th_properties['cols_to_predict'],
+                           feature_cols_to_use=th_properties['feature_cols_to_use'],
+                           index_cols=th_properties['index_cols'],
+                           normalize=False, feature_cols_to_normalize=None)
 
-    def retrieve_experiments_to_predict(self, variable_columns: list):
-        # computes full condition space and returns experiments that have no data and need to be predicted
-        unique_column_values = [self.data[column].unique() for column in variable_columns]
-        # inefficent can be combined with line above
-        for item in variable_columns:
-            print('Column {0}: contains {1} unqiue values'.format(item, len(self.data[item].unique())))
-
-        permutations = set(itertools.product(*unique_column_values))
-        temp_df = self.data[variable_columns].drop_duplicates()
-        current_experiments = set(zip(*[temp_df[column].values for column in variable_columns]))
-        experiments_to_predict = permutations - current_experiments
-        # current_experiments = list(current_experiments)
-        experiments_to_predict = list(experiments_to_predict)
-
-        print('Input dataframe contains {0} conditions out of {1} possible conditions\nThere are {2} conditions to be predicted'.format(
-            len(current_experiments), len(permutations), len(experiments_to_predict)))
-
-        return experiments_to_predict
-
-    def generate_experiments_to_predict(self, variable_columns: list):
-        # generates and returns a dataframe of experiments that lack data
-
-        experiments_to_predict = self.retrieve_experiments_to_predict(self.data, variable_columns)
-        predict_df = pd.DataFrame(experiments_to_predict, columns=variable_columns)
-
-        return predict_df
-
-    def return_sampled_df(self, percentage: int, variable_columns: list):
-        number_of_samples_to_select = float(percentage * 1e-2)
-        temp_df = self.data[variable_columns].drop_duplicates()
-        temp_df = temp_df.sample(frac=1).sample(frac=number_of_samples_to_select)
-
-        return temp_df
-
-    def build_progressive_sampling(self, variable_columns: list, num_runs=3, start_percent=25, end_percent=100, step_size=5):
-        for run in range(1, num_runs):
-            for percent in range(start_percent, end_percent, step_size):
-                sampled_df = self.return_sampled_df(self.data, percent, variable_columns)
-                test_df = self.data.loc[~self.data.index.isin(sampled_df.index)]
-                self.build_model(train_df=sampled_df, test_df=test_df, th_properties={})
-            # TODO: characterizaton has yet to include calculation of knee point
-
-    def rank_results(self, results_df, control_col, prediction_col, rank_name: str):
-        results_df[rank_name] = results_df[prediction_col] / results_df[control_col]
-        return results_df
-
+    # validation_data goes into here
     def evaluate_model(self, df, index_col_new_data, target_col_new_data, index_col_predictions_data, custom_metric=None):
         '''
         Take in a dataframe that was produced by the experiment and compare it with the predicted dataframe
@@ -121,60 +148,18 @@ class CombinatorialDesignModel:
             # TODO: ensure custom metric_can take at least two arguments
             return custom_metric(df_all[pred_col], df_all[target_col_new_data])
 
-    def retrieve_experiments_to_predict(self, variable_columns: list):
-        # computes full condition space and returns experiments that have no data and need to be predicted
-        unique_column_values = [self.data[column].unique() for column in variable_columns]
-        # inefficent can be combined with line above
-        for item in variable_columns:
-            print('Column {0}: contains {1} unqiue values'.format(item, len(self.data[item].unique())))
-
-        permutations = set(itertools.product(*unique_column_values))
-        temp_df = self.data[variable_columns].drop_duplicates()
-        current_experiments = set(zip(*[temp_df[column].values for column in variable_columns]))
-        experiments_to_predict = permutations - current_experiments
-        # current_experiments = list(current_experiments)
-        experiments_to_predict = list(experiments_to_predict)
-
-        print('Input dataframe contains {0} conditions out of {1} possible conditions\nThere are {2} conditions to be predicted'.format(
-            len(current_experiments), len(permutations), len(experiments_to_predict)))
-
-        return experiments_to_predict
-
-    def generate_experiments_to_predict(self, variable_columns: list):
-        # generates and returns a dataframe of experiments that lack data
-
-        experiments_to_predict = self.retrieve_experiments_to_predict(self.data, variable_columns)
-        predict_df = pd.DataFrame(experiments_to_predict, columns=variable_columns)
-
-        return predict_df
-
-    def return_sampled_df(self, percentage: int, variable_columns: list):
-        number_of_samples_to_select = float(percentage * 1e-2)
-        temp_df = self.data[variable_columns].drop_duplicates()
-        temp_df = temp_df.sample(frac=1).sample(frac=number_of_samples_to_select)
-
-        return temp_df
-
-    def build_progressive_sampling(self, variable_columns: list, num_runs=3, start_percent=25, end_percent=100, step_size=5):
-        for run in range(1, num_runs):
-            for percent in range(start_percent, end_percent, step_size):
-                sampled_df = self.return_sampled_df(self.data, percent, variable_columns)
-                test_df = self.data.loc[~self.data.index.isin(sampled_df.index)]
-                self.build_model(train_df=sampled_df, test_df=test_df, th_properties={})
-            # TODO: characterizaton has yet to include calculation of knee point
-
     def rank_results(self, results_df, control_col, prediction_col, rank_name: str):
         results_df[rank_name] = results_df[prediction_col] / results_df[control_col]
         return results_df
 
 
-class Host_Response_Model(Combinatorial_Design_Model):
+class Host_Response_Model(CombinatorialDesignModel):
 
     def __init__(self):
         pass
 
 
-class Fluorescence_Output_Model(Combinatorial_Design_Model):
+class Fluorescence_Output_Model(CombinatorialDesignModel):
 
     def __init__(self):
         pass
