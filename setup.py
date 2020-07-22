1be7b0c96c6a1a6b2ae9f389e8a578d853d82fc1
Hamed <eramian@netrias.com>
2020-07-22 13:14:27 -0400

    created setup.py and updated README.md
    Former-commit-id: 718ed8791ca0d578eb6f38fb0953f208998f82c7

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+from version import VERSION
+
+DISTNAME = 'cdm'
+DESCRIPTION = 'a tool for creating combinatorial design models and executing associated analyses'
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+MAINTAINER = 'netrias'
+MAINTAINER_EMAIL = 'eramian@netrias.com'
+
+setup(
+    name=DISTNAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author=MAINTAINER,
+    author_email=MAINTAINER_EMAIL,
+    packages=['cdm'] + ['cdm/' + s for s in find_packages('cdm')],
+    include_package_data=True,
+    install_requires=[
+        "keras",
+        "pandas",
+        "sklearn",
+        "seaborn"
+    ]
+)
